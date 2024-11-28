806fa3580a722fafb0c42bd6d6787dfd33e26095
Jonatanarvid <jonatan2004@hotmail.se>
2024-11-28 21:26:16 +0100

    I'm bad at physics :(

--- src/main/java/TestGrupp/Model/PhysicsComponent.java
+++ src/main/java/TestGrupp/Model/PhysicsComponent.java
@@ -3,16 +3,12 @@ package TestGrupp.Model;
 public class PhysicsComponent {
     private float velocityX;
     private float velocityY;
-    private float accelerationX;
-    private float accelerationY;
     private float forwardAcceleration;
-
     public PhysicsComponent() {
         this.velocityX = 0;
         this.velocityY = 0;
-        this.accelerationX = 0;
-        this.accelerationY = 0;
         this.forwardAcceleration = 0;
+
     }
 
     public float getVelocityX() {
@@ -31,22 +27,6 @@ public class PhysicsComponent {
         this.velocityY = velocityY;
     }
 
-    public float getAccelerationX() {
-        return accelerationX;
-    }
-
-    public void setAccelerationX(float accelerationX) {
-        this.accelerationX = accelerationX;
-    }
-
-    public float getAccelerationY() {
-        return accelerationY;
-    }
-
-    public void setAccelerationY(float accelerationY) {
-        this.accelerationY = accelerationY;
-    }
-
     public float getForwardAcceleration() {
         return forwardAcceleration;
     }
@@ -55,17 +35,14 @@ public class PhysicsComponent {
         this.forwardAcceleration = forwardAcceleration;
     }
 
-    public void update(float deltaTime, TransformComponent transform) {
-        // Update the velocity based on acceleration
-        velocityX += accelerationX * deltaTime;
-        velocityY += accelerationY * deltaTime;
 
-        // Update the velocity based on forward acceleration and rotation
+
+    public void update(float deltaTime, TransformComponent transform) {
+
         float radians = (float) Math.toRadians(transform.getRotation());
         velocityX += (float) (forwardAcceleration * Math.cos(radians) * deltaTime);
         velocityY += (float) (forwardAcceleration * Math.sin(radians) * deltaTime);
 
-        // Update the position based on velocity
         transform.setX(transform.getX() + velocityX * deltaTime);
         transform.setY(transform.getY() + velocityY * deltaTime);
     }
