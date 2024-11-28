8ba861c48281e4e2915e072bbe7996e5e1ab131f
Jonatanarvid <jonatan2004@hotmail.se>
2024-11-28 21:37:05 +0100

    Added friction to the physics system, since I noticed that was a factor affecting velocity in the original Asteroids game

--- src/main/java/TestGrupp/Model/PhysicsComponent.java
+++ src/main/java/TestGrupp/Model/PhysicsComponent.java
@@ -4,10 +4,13 @@ public class PhysicsComponent {
     private float velocityX;
     private float velocityY;
     private float forwardAcceleration;
+
+    private float friction;
     public PhysicsComponent() {
         this.velocityX = 0;
         this.velocityY = 0;
         this.forwardAcceleration = 0;
+        this.friction = 0.98f;
 
     }
 
@@ -35,6 +38,13 @@ public class PhysicsComponent {
         this.forwardAcceleration = forwardAcceleration;
     }
 
+    public float getFriction() {
+        return friction;
+    }
+
+    public void setFriction(float friction) {
+        this.friction = friction;
+    }
 
 
     public void update(float deltaTime, TransformComponent transform) {
@@ -42,6 +52,9 @@ public class PhysicsComponent {
         float radians = (float) Math.toRadians(transform.getRotation());
         velocityX += (float) (forwardAcceleration * Math.cos(radians) * deltaTime);
         velocityY += (float) (forwardAcceleration * Math.sin(radians) * deltaTime);
+
+        velocityX *= friction;
+        velocityY *= friction;
 
         transform.setX(transform.getX() + velocityX * deltaTime);
         transform.setY(transform.getY() + velocityY * deltaTime);
