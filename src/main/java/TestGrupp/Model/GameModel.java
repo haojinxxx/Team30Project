23cf6b9b98596dbad8cb2a77fa5a94296e82728b
Jonatanarvid <jonatan2004@hotmail.se>
2024-11-28 23:05:52 +0100

    Added method for spawning asteroid entities

--- src/main/java/TestGrupp/Model/GameModel.java
+++ src/main/java/TestGrupp/Model/GameModel.java
@@ -1,5 +1,30 @@
 package TestGrupp.Model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameModel {
-    public Ship player;
+    private List<GameObject> gameObjects;
+
+    public GameModel() {
+        this.gameObjects = new ArrayList<>();
+    }
+
+    public void addGameObject(GameObject gameObject) {
+        this.gameObjects.add(gameObject);
+    }
+
+    public void removeGameObject(GameObject gameObject) {
+        this.gameObjects.remove(gameObject);
+    }
+
+    public List<GameObject> getGameObjects() {
+        return gameObjects;
+    }
+
+    // Method to create a new Asteroid
+    public void createAsteroid(float x, float y, float rotation, float scaleX, float scaleY, double speed, int health, int childAsteroids) {
+        Asteroid asteroid = new Asteroid(x, y, rotation, scaleX, scaleY, speed, health, childAsteroids);
+        addGameObject(asteroid);
+    }
 }
