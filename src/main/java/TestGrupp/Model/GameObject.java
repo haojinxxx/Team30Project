c732cb28ff27abc85fc00c83c808e31ddcd78a72
Jonatanarvid <jonatan2004@hotmail.se>
2024-11-28 21:10:23 +0100

    Wrote abstract GameObject class. The handling of scale of objects and positions have been moved into its own Transform component.

--- src/main/java/TestGrupp/Model/GameObject.java
+++ src/main/java/TestGrupp/Model/GameObject.java
@@ -1,8 +1,43 @@
 package TestGrupp.Model;
 
-public abstract class GameObject  {
+public abstract class GameObject {
+    private int id;
+    private boolean active;
+    private TransformComponent transform;
+    private PhysicsComponent physics;
 
+    public GameObject(float initialX, float initialY, float rotation, float scaleX, float scaleY) {
+        this.id = System.identityHashCode(this);
+        this.active = true;
+        this.transform = new TransformComponent(initialX, initialY, rotation, scaleX, scaleY);
+        this.physics = new PhysicsComponent();
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public boolean isActive() {
+        return active;
+    }
 
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public TransformComponent getTransform() {
+        return transform;
+    }
+
+    public PhysicsComponent getPhysics() {
+        return physics;
+    }
+
+    // Main update method in GameObject class
+    public void update(float deltaTime) {
+        if (!active) return;  // Skip updating if the object is inactive
+
+        // Update other components, like physics
+        physics.update(deltaTime, transform);  // Physics updates the TransformComponent's data
+    }
 }
