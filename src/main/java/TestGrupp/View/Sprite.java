f4abc85aaeeb3bb193f11d60de33b65cbf8fa934
Jonatanarvid <jonatan2004@hotmail.se>
2024-12-06 02:26:00 +0100

    Class extending JComponent for handling rotation of sprites

--- src/main/java/TestGrupp/View/Sprite.java
+++ src/main/java/TestGrupp/View/Sprite.java
@@ -3,22 +3,50 @@ package TestGrupp.View;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
 
 public class Sprite extends JComponent {
     private double rotation;
+    private BufferedImage image;
 
+    // Constructor now only accepts an image when available
+    public Sprite() {
+        setOpaque(false); // Make sure the sprite component is transparent
+    }
+
+    // Method to set the image for the sprite
+    public void setImage(BufferedImage image) {
+        this.image = image;
+        repaint(); // Repaint after the image is set
+    }
+
+    // Method to set the rotation angle
     public void setRotation(double rotation) {
         this.rotation = rotation;
-        repaint();
+        //repaint(); // Repaint after the rotation is set
+    }
+    
+    public double getRotation() {
+        return rotation;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        Graphics2D g2d = (Graphics2D) g;
-        AffineTransform originalTransform = g2d.getTransform();
-        g2d.rotate(Math.toRadians(rotation), getWidth() / 2, getHeight() / 2);
-        // Draw the component (e.g., an image or shape)
-        g2d.setTransform(originalTransform);
+        if (image != null) {
+            Graphics2D g2d = (Graphics2D) g;
+            AffineTransform originalTransform = g2d.getTransform();  // Save the original transform
+
+            // Rotate around the center of the component
+            int centerX = getWidth() / 2;
+            int centerY = getHeight() / 2;
+            g2d.rotate(rotation, centerX, centerY);  // Rotate by the current angle
+
+            // Draw the image centered in the component
+            g2d.drawImage(image, 0, 0, this);
+
+            // Restore the original transform after drawing
+            g2d.setTransform(originalTransform);
+        }
     }
 }
