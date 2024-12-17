0e0b0e93f1158dfce1f92c5762bd8e3b36bc836d
Jonatanarvid <jonatan2004@hotmail.se>
2024-12-17 05:07:41 +0100

    We don't want static accessing of the playership, I think that may cause issues if the player dies and respawns.

--- src/main/java/TestGrupp/Controller/InputProcessor.java
+++ src/main/java/TestGrupp/Controller/InputProcessor.java
@@ -7,12 +7,14 @@ import java.awt.event.KeyEvent;
 public class InputProcessor {
 
     private final SoundManager soundManager;
-    private InputHandler inputHandler;
+    private final InputHandler inputHandler;
+    private final GameModel gameModel;
 
     private long lastFireTime;
-    private long fireCooldown;
+    private final long fireCooldown;
 
     public InputProcessor(GameModel gameModel, InputHandler inputHandler, SoundManager soundManager, long fireCooldown) {
+        this.gameModel = gameModel;
         this.inputHandler = inputHandler;
         this.fireCooldown = fireCooldown;
         this.soundManager = soundManager;
@@ -26,19 +28,19 @@ public class InputProcessor {
 
     private void processRotationInput() {
         if (inputHandler.isKeyPressed(KeyEvent.VK_A)) {
-            GameModel.getPlayerShip().rotate(-Math.PI / 30);
+            gameModel.getPlayerShip().rotate(-Math.PI / 30);
         }
         if (inputHandler.isKeyPressed(KeyEvent.VK_D)) {
-            GameModel.getPlayerShip().rotate(Math.PI / 30);
+            gameModel.getPlayerShip().rotate(Math.PI / 30);
         }
     }
 
     private void processForwardMovement() {
         if (inputHandler.isKeyPressed(KeyEvent.VK_W)) {
-            GameModel.getPlayerShip().setMovingForward(true);
+            gameModel.getPlayerShip().setMovingForward(true);
             soundManager.playThrusterSound();
         } else {
-            GameModel.getPlayerShip().setMovingForward(false);
+            gameModel.getPlayerShip().setMovingForward(false);
             soundManager.stopThrusterSound();
         }
     }
@@ -47,7 +49,7 @@ public class InputProcessor {
         if (inputHandler.isKeyPressed(KeyEvent.VK_SPACE)) {
             long currentTime = System.currentTimeMillis();
             if (currentTime - lastFireTime >= fireCooldown) {
-                GameModel.getPlayerShip().fire();
+                gameModel.getPlayerShip().fire();
                 soundManager.playFireSound();
                 lastFireTime = currentTime;
             }
--- src/main/java/TestGrupp/Model/GameModel.java
+++ src/main/java/TestGrupp/Model/GameModel.java
@@ -153,7 +153,7 @@ public class GameModel implements GameEventListener, Subject  {
 
 
 
-    public static PlayerShip getPlayerShip() {
+    public PlayerShip getPlayerShip() {
         return playerShip;
     }
 
