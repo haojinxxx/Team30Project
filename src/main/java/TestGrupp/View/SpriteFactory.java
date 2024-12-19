9d372c6adee46041b089af593b5864178d772064
Jonatanarvid <jonatan2004@hotmail.se>
2024-12-19 02:29:16 +0100

    Fixes so that previous codes work with the change to ConfigurationLoader being an utility class

--- src/main/java/TestGrupp/View/SpriteFactory.java
+++ src/main/java/TestGrupp/View/SpriteFactory.java
@@ -1,10 +1,12 @@
 package TestGrupp.View;
 
-public class SpriteFactory {
-    private ConfigurationLoader configLoader;
+import TestGrupp.Controller.ConfigurationLoader;
 
-    public SpriteFactory(ConfigurationLoader configLoader) {
-        this.configLoader = configLoader;
+public class SpriteFactory {
+
+
+    public SpriteFactory() {
+
     }
 
     public Sprite createSprite(String spriteType) {
@@ -12,29 +14,29 @@ public class SpriteFactory {
         switch (spriteType) {
             case "PlayerShip":
                 System.out.println("Creating PlayerShip sprite...");
-                PlayerShipSprite playerShipSprite = new PlayerShipSprite(configLoader.getProperty("player.width"), configLoader.getProperty("player.height"));
-                playerShipSprite.setBounds(0, 0, configLoader.getProperty("player.width"), configLoader.getProperty("player.height")); // Set bounds explicitly
+                PlayerShipSprite playerShipSprite = new PlayerShipSprite(ConfigurationLoader.getProperty("player.width"), ConfigurationLoader.getProperty("player.height"));
+                playerShipSprite.setBounds(0, 0, ConfigurationLoader.getProperty("player.width"), ConfigurationLoader.getProperty("player.height")); // Set bounds explicitly
                 return playerShipSprite;
             case "PlayerProjectile":
                 System.out.println("Creating PlayerProjectile sprite...");
-                PlayerProjectileSprite playerProjectileSprite = new PlayerProjectileSprite(configLoader.getProperty("projectile.width"), configLoader.getProperty("projectile.height"));
-                playerProjectileSprite.setBounds(0, 0, configLoader.getProperty("projectile.width"), configLoader.getProperty("projectile.height")); // Set bounds explicitly
+                PlayerProjectileSprite playerProjectileSprite = new PlayerProjectileSprite(ConfigurationLoader.getProperty("projectile.width"), ConfigurationLoader.getProperty("projectile.height"));
+                playerProjectileSprite.setBounds(0, 0, ConfigurationLoader.getProperty("projectile.width"), ConfigurationLoader.getProperty("projectile.height")); // Set bounds explicitly
                 return playerProjectileSprite;
             case "PowerUp":
                 System.out.println("Creating PowerUp sprite...");
-                PowerUpSprite powerUpSprite = new PowerUpSprite(configLoader.getProperty("powerup.width"), configLoader.getProperty("powerup.height"));
-                powerUpSprite.setBounds(0, 0,configLoader.getProperty("powerup.width"), configLoader.getProperty("powerup.height")); // Set bounds explicitly
+                PowerUpSprite powerUpSprite = new PowerUpSprite(ConfigurationLoader.getProperty("powerup.width"), ConfigurationLoader.getProperty("powerup.height"));
+                powerUpSprite.setBounds(0, 0,ConfigurationLoader.getProperty("powerup.width"), ConfigurationLoader.getProperty("powerup.height")); // Set bounds explicitly
                 return powerUpSprite;
             case "Asteroid":
                 System.out.println("Creating Asteroid sprite...");
-                AsteroidSprite asteroidSprite = new AsteroidSprite(configLoader.getProperty("asteroid.width"), configLoader.getProperty("asteroid.height"));
-                asteroidSprite.setBounds(0, 0, configLoader.getProperty("asteroid.width"), configLoader.getProperty("asteroid.height")); // Set bounds explicitly
+                AsteroidSprite asteroidSprite = new AsteroidSprite(ConfigurationLoader.getProperty("asteroid.width"), ConfigurationLoader.getProperty("asteroid.height"));
+                asteroidSprite.setBounds(0, 0, ConfigurationLoader.getProperty("asteroid.width"), ConfigurationLoader.getProperty("asteroid.height")); // Set bounds explicitly
                 return asteroidSprite;
 
             case "EnemyShip":
                 System.out.println("Creating EnemyShip sprite...");
-                EnemyShipSprite enemyShip = new EnemyShipSprite(configLoader.getProperty("enemy.width"), configLoader.getProperty("enemy.height"));
-                enemyShip.setBounds(0, 0, configLoader.getProperty("enemy.width"), configLoader.getProperty("enemy.height")); // Set bounds explicitly
+                EnemyShipSprite enemyShip = new EnemyShipSprite(ConfigurationLoader.getProperty("enemy.width"), ConfigurationLoader.getProperty("enemy.height"));
+                enemyShip.setBounds(0, 0, ConfigurationLoader.getProperty("enemy.width"), ConfigurationLoader.getProperty("enemy.height")); // Set bounds explicitly
                 return enemyShip;
 
 
@@ -42,8 +44,8 @@ public class SpriteFactory {
 
             case "EnemyProjectile":
                 System.out.println("Creating EnemyProjectile sprite...");
-                EnemyProjectileSprite enemyProjectile = new EnemyProjectileSprite( configLoader.getProperty("projectile.width"), configLoader.getProperty("projectile.height"));
-                enemyProjectile.setBounds(0, 0,  configLoader.getProperty("projectile.width"),  configLoader.getProperty("projectile.height")); // Set bounds explicitly
+                EnemyProjectileSprite enemyProjectile = new EnemyProjectileSprite( ConfigurationLoader.getProperty("projectile.width"), ConfigurationLoader.getProperty("projectile.height"));
+                enemyProjectile.setBounds(0, 0,  ConfigurationLoader.getProperty("projectile.width"),  ConfigurationLoader.getProperty("projectile.height")); // Set bounds explicitly
                 return enemyProjectile;
             default:
                 System.out.println("Unknown sprite type: " + spriteType);
